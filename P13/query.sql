7beda2bc18cc80a05f177ba1e5452f344da6ca03
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2019-10-30 22:49:00 +0700

    update Pertemuan 13 (SEBELOM UTS)

--- P13/query.sql
+++ P13/query.sql
@@ -17,7 +17,10 @@ CREATE TABLE `RincianBiaya`(
     Tanggal DATE NOT NULL,
     Kelompok VARCHAR(50) NOT NULL,
     SubKelompok VARCHAR(50) NOT NULL,
-    Jumlah INT
+    Jumlah INT,
+
+    -- EXAMPLE FOREIGN KEY HERE !!
+    FOREIGN KEY (NomorPesanan) REFERENCES KartuPesanan(NomorPesanan)
 );
 DESC `RincianBiaya`;
 
@@ -28,7 +31,7 @@ INSERT INTO
     VALUES
         (1, 'Sepatu', 4000, '2004-01-01', '2004-01-30', 'PT Karya'),
         (2, 'Sandal', 3000, '2004-02-02', '2004-02-28', 'PT Abdi'),
-        (3, 'Sandal', 3000, '2004-03-03', '2004-03-30', 'PT Maju');
+        (3, 'Tas', 3000, '2004-03-03', '2004-03-30', 'PT Maju');
 
 -- CHECK DATA KartuPesanan
 SELECT * FROM `KartuPesanan`;
@@ -46,6 +49,7 @@ INSERT
     VALUES
    	(1, '2004-01-15', 'Material', 'Kulit', 10000000),
     (1, '2004-01-15', 'Material', 'Kain', 5000000),
+    (1, '2004-02-15', 'Tenaga Kerja', 'Upah Buruh', 30000000),
     
     (2, '2004-02-15', 'Material', 'Kulit', 20000000),
     (2, '2004-02-15', 'Material', 'Kain', 10000000),
@@ -58,7 +62,84 @@ INSERT
 
 SELECT * FROM `RincianBiaya`;
 
+
+/*
+ * STUDI KASUS
+ */
+
 /* Laporan Biaya Langsung Per Pesanan Diperinci Sampai Kelompok Biaya */
+SELECT 
+	pesanan.NomorPesanan,
+    pesanan.JenisProduk,
+    pesanan.JmlPesanan,
+    SUM(rincian.Jumlah) as `Kelompok Biaya`,
+    (SUM(rincian.Jumlah) * pesanan.JmlPesanan) as `Jumlah Biaya`
+FROM 
+	RincianBiaya as rincian
+	JOIN
+    	KartuPesanan as pesanan ON 
+            pesanan.NomorPesanan = rincian.NomorPesanan
+GROUP BY
+	rincian.NomorPesanan,
+    rincian.Kelompok;
+
+/*  Laporan Biaya Langsung Per Bulan */
+SELECT 
+	DATE_FORMAT(TglPesanan, '%Y') as Tahun, -- OY! EXAMPLE TANGGAL HERE!!
+    DATE_FORMAT(TglPesanan, '%m') as Bulan, 
+    SUM(rincian.Jumlah) as `Kelompok Biaya`,
+    (SUM(rincian.Jumlah) * pesanan.JmlPesanan) as `Jumlah Biaya`
+FROM 
+	RincianBiaya as rincian
+	JOIN
+    	KartuPesanan as pesanan ON 
+            pesanan.NomorPesanan = rincian.NomorPesanan
+GROUP BY
+    Tahun,
+    Bulan;
+
+
+/*  Laporan Biaya Langsung Per Produk */
+SELECT 
+	pesanan.JenisProduk as Produk,
+    SUM(rincian.Jumlah) as `Kelompok Biaya`,
+    (SUM(rincian.Jumlah) * pesanan.JmlPesanan) as `Jumlah Biaya`
+FROM 
+	RincianBiaya as rincian
+	JOIN
+    	KartuPesanan as pesanan ON 
+            pesanan.NomorPesanan = rincian.NomorPesanan
+GROUP BY
+    Produk;
+
+
+/* Penghitungan Biaya Produk Per Pesanan */
+SELECT 
+	pesanan.NomorPesanan,
+    pesanan.JenisProduk,
+    pesanan.JmlPesanan,
+    (SUM(rincian.Jumlah) * pesanan.JmlPesanan) as `Biaya Langsung`,
+    (
+        SUM(rincian.Jumlah) * pesanan.JmlPesanan + (SUM(rincian.Jumlah) * pesanan.JmlPesanan * 30 / 100)
+    ) 
+    as `Biaya Langsung`,
+    (
+        (SUM(rincian.Jumlah) * pesanan.JmlPesanan) +
+        SUM(rincian.Jumlah) * pesanan.JmlPesanan + (SUM(rincian.Jumlah) * pesanan.JmlPesanan * 30 / 100)
+    ) as `Total Biaya`,
+    SUM(rincian.Jumlah) as `BiayaPerUnit`
+FROM 
+	RincianBiaya as rincian
+	JOIN
+    	KartuPesanan as pesanan ON 
+            pesanan.NomorPesanan = rincian.NomorPesanan
+GROUP BY
+	rincian.NomorPesanan;
+
+/* 
+ * 
+ * Laporan Total Biaya, Rata-rata, Jumlah Tertinggi, Jumlah Terkecil, serta Jumlah Pesanan yang menggunakan biaya 
+ */
 
 SELECT 
 	pesanan.NomorPesanan,
@@ -73,4 +154,4 @@ FROM
             pesanan.NomorPesanan = rincian.NomorPesanan
 GROUP BY
 	rincian.NomorPesanan,
-    rincian.Kelompok
+    rincian.Kelompok;
