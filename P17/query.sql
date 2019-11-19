05c4a7edf20cb264a49782f8a89c3ef1817a8bfc
Abdul Aziz Al Basyir <abdulazizalbasyir119@gmail.com>
2019-11-19 19:38:43 +0700

    UPDATE FIX

--- P17/query.sql
+++ P17/query.sql
@@ -8,24 +8,40 @@ CREATE TABLE `post`(
 );
 
 DELIMITER |
-CREATE FUNCTION `symbol_pattern_getter`(msg TEXT, symbol TEXT) 
+CREATE FUNCTION `symbol_pattern_getter`(TEXTDATA TEXT, SYMBOL TEXT) 
 RETURNS TEXT
 -- DETERMINISTIC
     BEGIN
-        DECLARE idx INT;
-        DECLARE hasil TEXT;
-        DECLARE KATA TEXT;
+        -- Menyiapkan varibale hasil
+        DECLARE RESULT TEXT;
+        DECLARE LENGTH_TEXT INT;
+        DECLARE TEXT_ON_SEARCHER TEXT;
+        DECLARE INDEX_FOR_SEARCHING INT;
+        DECLARE LAST_TEXT_IN_SEARCHING TEXT;
 
-        SET idx = 0;
-        SET hasil = "";
+        -- set hasil tanpa string
+        SET RESULT = "";
 
+        -- Menyiapkan data maksimum teks
+        SET LENGTH_TEXT = LENGTH(TEXTDATA);
+
+        -- Membuat Index dari 0
+        SET INDEX_FOR_SEARCHING = 0;
+
+        -- MEMULAI PENCARIAN
         REPEAT
-            SET idx = idx + 1;
-            SET KATA = SUBSTRING_INDEX(msg, ' ', idx);
+            SET INDEX_FOR_SEARCHING = INDEX_FOR_SEARCHING + 1;
+            SET TEXT_ON_SEARCHER = SUBSTRING_INDEX(TEXTDATA, ' ', INDEX_FOR_SEARCHING);
+            SET LAST_TEXT_IN_SEARCHING = SUBSTRING_INDEX(TEXT_ON_SEARCHER, ' ', -1); 
 
-        UNTIL KATA IS NULL END REPEAT;
+            IF LEFT(LAST_TEXT_IN_SEARCHING, 1) = SYMBOL THEN
+                SET RESULT = CONCAT(RESULT, LAST_TEXT_IN_SEARCHING, ', ');
+            END IF;
 
-        RETURN hasil;
+        UNTIL (LENGTH(TEXT_ON_SEARCHER) >= LENGTH_TEXT) END REPEAT;
+
+        -- Mengembalikan hasil dari pencarian
+        RETURN RESULT;
     END |   
 DELIMITER ;
 
